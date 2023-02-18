a138286c9e6e8cc3f65ce1f616f8dafba0810266
MD Mobin <74850678+Djsmk123@users.noreply.github.com>
2023-02-18 14:51:05 +0530

    add test for model

---
+++ lib/features/number_trivia/data/models/number_trivia_model.dart
@@ -0,0 +1,12 @@
+import 'package:mvvm_movies_app/features/number_trivia/domain/entities/number_trivia.dart';
+
+class NumberTriviaModel extends NumberTrivia {
+  const NumberTriviaModel({required super.number, required super.text});
+  factory NumberTriviaModel.fromJson(Map<String, dynamic> json) {
+    return NumberTriviaModel(
+        number: (json['number'] as num).toInt(), text: json['text']);
+  }
+  Map<String, dynamic> toJson() {
+    return {'text': text, 'number': number};
+  }
+}
---
+++ test/features/number_trivia/data/number_trivia_model.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mvvm_movies_app/features/number_trivia/data/models/number_trivia_model.dart';
+import 'package:mvvm_movies_app/features/number_trivia/domain/entities/number_trivia.dart';
+
+import '../../../Fixtures/fixture_reader.dart';
+
+void main() {
+  const tNumberTriviaModel = NumberTriviaModel(text: "Test text", number: 1);
+  test('should be subclass of Number Entity', () {
+    expect(tNumberTriviaModel, isA<NumberTrivia>());
+  });
+  group('fromJson', () {
+    test('should return a valid model when the json number is an integer',
+        () async {
+      //arrange
+      final Map<String, dynamic> jsonMap = json.decode(fixture('trivia'));
+      //act
+      final result = NumberTriviaModel.fromJson(jsonMap);
+      expect(result, tNumberTriviaModel);
+      //assert
+    });
+    test('should return valid model when the json number is integer', () async {
+      final Map<String, dynamic> jsonMap =
+          json.decode(fixture('trivia_double'));
+      final result = NumberTriviaModel.fromJson(jsonMap);
+      expect(result, tNumberTriviaModel);
+    });
+  });
+  group('toJson', () {
+    test('should return  a json map containing the proper data', () async {
+      final result = tNumberTriviaModel.toJson();
+      final expectedMap = {'text': "Test text", 'number': 1};
+      expect(result, expectedMap);
+    });
+  });
+}
